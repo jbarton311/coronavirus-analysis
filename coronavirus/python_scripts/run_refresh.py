3c7213ecb87e8a6ebd20ed9e477b039558efd680
Jeff B <vtbandofhorses@gmail.com>
2020-03-20 09:26:03 -0400

    Adding a main executor script

---
+++ coronavirus/python_scripts/run_refresh.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+import os
+import logging
+import pandas as pd
+from pycountry import countries
+
+from hopkins_cleaner import HopkinsDataFull
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Adding a stream handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+
+LOGGER.addHandler(ch)
+
+def test_us_province_counts(df):
+    """
+    Make sure all province and states have same number
+    of records
+    """
+    us = df.loc[df['country_or_region'] == 'US']
+
+    grp = us.groupby(['province_or_state'])['daily_new_cases'].size().reset_index()
+
+    # Make sure all state names have same number of records
+    assert grp['daily_new_cases'].min() == grp['daily_new_cases'].max()
+
+def test_daily_vs_total_logic(df):
+    max_date = df['date'].max()
+    yesterday = df.loc[df['date'] == max_date]
+    
+    LOGGER.info(f"Total Cases: {df['daily_new_cases'].sum()}")
+    LOGGER.info(f"Total Recoveries: {df['daily_new_recoveries'].sum()}")
+    LOGGER.info(f"Total Deaths: {df['daily_new_deaths'].sum()}")                
+                
+    assert yesterday['running_total_cases'].sum() == df['daily_new_cases'].sum()
+    assert yesterday['running_total_recoveries'].sum() == df['daily_new_recoveries'].sum()
+    assert yesterday['running_total_deaths'].sum() == df['daily_new_deaths'].sum()
+
+LOGGER.info("START")
+
+# Execute main logic
+self = HopkinsDataFull()
+self.run()
+df = self.data
+
+# Execute test cases
+test_us_province_counts(df)
+test_daily_vs_total_logic(df)
+
+# Save CSV
+df.to_csv('../output_data/HOPKINS_CLEANED.csv', index=False)
